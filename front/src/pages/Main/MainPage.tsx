7d888f678c13400a0f234e33683b60275417bef0
Yujin031009 <115140447+Yujin031009@users.noreply.github.com>
2024-11-20 19:45:19 +0900

    #06 [front] backend 통신 수정

--- front/src/pages/Main/MainPage.tsx
+++ front/src/pages/Main/MainPage.tsx
@@ -1,28 +1,36 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { CommonSection } from "../../components/CommonSection/CommonSection";
 import { Header } from "../../components/Header";
 import { Root, StockList } from "./styled";
 
 export const MainPage = () => {
   const [stockData, setStockData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch("http://127.0.0.1:8000/test")
-      .then((response) => response.json())
-      .then((stockData) => setStockData(stockData.message))
-      .catch((error) => console.error("Error:", error));
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://127.0.0.1:8000/test"); // Flask API 호출
+        setStockData(response.data); // 데이터를 상태에 저장
+        setLoading(false); // 로딩 완료
+      } catch (err: any) {
+        setError(err); // 에러 처리
+        setLoading(false);
+      }
+    };
+
+    fetchData();
   }, []);
+
+  if (loading) return <div>Loading...</div>; // 로딩 중일 때
+  if (error) return <div>Error: {error}</div>; // 에러 발생 시
 
   return (
     <Root>
       <Header showLogo={true} />
-      {stockData ? (
-        <CommonSection>
-          <StockList>{stockData}</StockList>
-        </CommonSection>
-      ) : (
-        <CommonSection>Loading</CommonSection>
-      )}
+      {stockData && <CommonSection>Name: {stockData}</CommonSection>}
     </Root>
   );
 };
